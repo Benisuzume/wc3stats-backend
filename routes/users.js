f115711d70d3e2b35b6de88b121718cc52d2bee4
Tiago <tiagor@kth.se>
2019-10-29 22:43:23 +0100

    added stats command

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const UsersDao = require("../controllers/db/users-dao");
 
 router.get('/', function(req, res, next) {
   res.send('resource');
 });
 
+function sendResponseObject(res, statusCode, object) {
+  res.writeHead(statusCode, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(object));
+}
 
 /**
  * Returns all users
@@ -23,5 +28,29 @@ router.get('/all', function(req, res, next) {
   });
 });
 
+/**
+ * Queries for a user by a certain name includes ranking.
+ * Example query: { "name": "pinzu"}
+ */
+router.get('/name', function(req, res, next) {
+  var name = req.body.name.toLowerCase();
+  var includeRank = true; // default
+  (async () => {  
+    const user = await UsersDao.getUserByName(name);
+    if (includeRank !== undefined && includeRank) {
+        const [teamRank, soloRank, ffaRank] = await Promise.all([ 
+        UsersDao.getUserTeamRank(name), 
+        UsersDao.getUserSoloRank(name), 
+        UsersDao.getUserFFARank(name) ]) 
+        user.teamRank = teamRank;
+        user.soloRank = soloRank;
+        user.ffaRank = ffaRank;
+    }
+    if (user != null) 
+      sendResponseObject(res, 200, user);
+    else 
+      sendResponseObject(res, 404, user);
+    })();
+});
 
 module.exports = router;
